71ac11b542075fa350fecd0b0d2b70c36b93c789
geekact <geekacting@gmail.com>
2025-07-14 18:13:20 +0800

    fix: enhance pickObjectOwner function to handle different response structures

--- src/sui/pick-object-owner.ts
+++ src/sui/pick-object-owner.ts
@@ -1,14 +1,17 @@
-import type { SuiObjectResponse } from '@mysten/sui/client';
+import type { ObjectOwner, SuiObjectResponse } from '@mysten/sui/client';
 
 /**
  * Pick **AddressOwner** or **ObjectOwner** from object. `Immutable` owner will be ignored.
  */
-export const pickObjectOwner = (response: SuiObjectResponse): string | undefined => {
+export const pickObjectOwner = (
+  response: SuiObjectResponse | { owner: ObjectOwner | null },
+): string | undefined => {
   if ('error' in response && response.error) {
     throw new Error(`Object with error: ${response.error?.code}`);
   }
 
-  const owner = response.data?.owner;
+  const owner =
+    'data' in response ? response.data?.owner : 'owner' in response ? response.owner : null;
 
   if (!owner || owner === 'Immutable') return;
   if ('AddressOwner' in owner) return owner.AddressOwner;
